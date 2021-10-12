82f4bba346d99bc0c19b84df9f7ba1be84558084
Phanitapu <venkateswarlu.phanitapu@concentrix.com>
2021-10-12 15:46:46 +0530

    first commit for iOS

---
+++ UIKitSample/CustomTabBarView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabBarView.swift
+//  UIKitSample
+//
+//  Created by Shruthi Vittal on 14/06/21.
+//
+
+import SwiftUI
+
+public struct CustomTabBarView: View {
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomTabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabBarView()
+    }
+}
---
+++ UIKitSample/UIKitSample.h
@@ -0,0 +1,18 @@
+//
+//  UIKitSample.h
+//  UIKitSample
+//
+//  Created by Shruthi Vittal on 14/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIKitSample.
+FOUNDATION_EXPORT double UIKitSampleVersionNumber;
+
+//! Project version string for UIKitSample.
+FOUNDATION_EXPORT const unsigned char UIKitSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitSample/PublicHeader.h>
+
+
